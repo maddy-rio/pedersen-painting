5d43a14e24dbd3c3b9209cedb3378f0f9647fa17
Maddy Rio <madison-rio@hotmail.com>
2024-05-07 18:40:28 +1200

    Add landing page and react framework

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,7 +1,7 @@
 function footer() {
  return (
     <section id="section-footer">
-        <p className="footer">© 2024 | Designed & coded by Maddy Rio </p>
+       
     </section>
  )
 }
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,13 +1,26 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-
-
-// Standard nav bar
+// Nav bar
 function Nav() {
   return (
-    <header className="nav">
-      <Sidebar />
- 
-    </header>
+   
+    <section id="nav-bar">
+      <img href="/"class="nav-image" src="../../public/nav.png"/>
+      <ul class="menu">
+      <li><a class="menuItem" href="#">About us</a></li>
+       <li><a class="menuItem" href="#">Our work</a></li>
+      <li><a class="menuItem" href="#">Services</a></li>
+      <li><a class="menuItem" href="#">Testimonials</a></li>
+      <li><a class="menuItem" href="#">Contact</a></li>
+     </ul>
+     <button class="hamburger">
+     <div>
+      <MenuIcon style={{ fontSize: '45px'}}/>
+      <CloseIcon style={{ display: 'none'}} />
+    </div>
+    </button> 
+    </section>
   )
 }
 

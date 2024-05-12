01f8022d7819d0fc9e2b634aa11b44e39e83c997
Maddy Rio <madison-rio@hotmail.com>
2024-05-12 20:50:06 +1200

    Add contact form inputs and css

--- client/components/pages/Contact.jsx
+++ client/components/pages/Contact.jsx
@@ -7,11 +7,15 @@ function handleSubmit(event) {
   
     emailjs.sendForm('service_co61l7f', 'template_jvtxwbm', event.target, 'w5N7m0Zq4jOQsPjZO')
       .then((result) => {
-        console.log('Email sent successfully:', result.text);
+            alert('Thanks! We will be in touch soon', result.text);
+            window.location.reload
       }, (error) => {
-        console.error('Email sending failed:', error.text);
+        alert('Something went wrong!', error.text);
+        window.location.reload
       });
   }
+
+//   Add a custom alert message to the page to replace alerts
 
 function Contact() {
 
@@ -20,8 +24,8 @@ function Contact() {
           <Nav />
            <section>
            <h1 class="page-header">Contact us</h1>
-           <p>Fill out the form below or <a class="contact-email" href="mailto:jayden@pedersenpainting.co.nz">email</a> us to get in touch</p>
-           <form onSubmit={handleSubmit}>
+           <p>Fill out the form below to get in touch</p>
+           <form class="contact-form" onSubmit={handleSubmit}>
       <input type="name" name="name" placeholder="Your Name" required />
       <input type="email" name="email" placeholder="Your Email" required />
       <input type="phone" name="phone" placeholder="Your number" required />

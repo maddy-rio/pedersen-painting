210045ae1dbd022d5bb4b48ec06f1253d7f9d85e
Maddy Rio <madison-rio@hotmail.com>
2024-06-24 19:55:42 +1200

    Fix carousel opening on work page

--- client/components/pages/OurWork.jsx
+++ client/components/pages/OurWork.jsx
@@ -37,54 +37,54 @@ function OurWork() {
         <section id="work-page">
           <section id="work-section">
             <div class="work-flex-container">
-              <div class="work-flex-item">
-                <div class="image-container">
-                  <img src="/Haumoana.jpeg" alt="House in Haumoana" />
-                  <div class="static-overlay"></div>
-                  <div onClick={() => toggleModal(<Carousel />)} class="overlay"></div>
-                  <div class="text">Haumoana</div>
-                </div>
-              </div>
-              <div class="work-flex-item">
-                <div class="image-container">
-                  <img src="/Edenlane.jpeg" alt="House on Eden Lane" />
-                  <div class="static-overlay"></div>
-                  <div onClick={() => toggleModal(<Carousel2 />)} class="overlay"></div>
-                  <div class="text">Eden Lane</div>
-                </div>
-              </div>
-              <div class="work-flex-item">
-                <div class="image-container">
-                  <img src="/Waimarama.jpeg" alt="House in Waimārama" />
-                  <div class="static-overlay"></div>
-                  <div onClick={() => toggleModal(<Carousel3 />)} class="overlay"></div>
-                  <div class="text">Waimārama</div>
-                </div>
-              </div>
-              <div class="work-flex-item">
-                <div class="image-container">
-                  <img src="/Taihape.jpeg" alt="House in Taihape" />
-                  <div class="static-overlay"></div>
-                  <div onClick={() => toggleModal(<Carousel4 />)} class="overlay"></div>
-                  <div class="text">Taihape</div>
-                </div>
-              </div>
-              <div class="work-flex-item">
-                <div class="image-container">
-                  <img src="/Interiors.jpeg" alt="Interior pictures" />
-                  <div class="static-overlay"></div>
-                  <div onClick={() => toggleModal(<Carousel5 />)} class="overlay"></div>
-                  <div class="text">Interiors</div>
-                </div>
-              </div>
-              <div class="work-flex-item">
-                <div class="image-container">
-                  <img src="/Exteriors.jpeg" alt="Exterior pictures" />
-                  <div class="static-overlay"></div>
-                  <div onClick={() => toggleModal(<Carousel6 />)} class="overlay"></div>
-                  <div class="text">Exteriors</div>
-                </div>
-              </div>
+            <div class="work-flex-item">
+  <div class="image-container" onClick={() => toggleModal(<Carousel />)}>
+    <img src="/Haumoana.jpeg" alt="House in Haumoana" />
+    <div class="static-overlay"></div>
+    <div class="overlay"></div>
+    <div class="text">Haumoana</div>
+  </div>
+</div>
+<div class="work-flex-item">
+  <div class="image-container" onClick={() => toggleModal(<Carousel2 />)}>
+    <img src="/Edenlane.jpeg" alt="House on Eden Lane" />
+    <div class="static-overlay"></div>
+    <div class="overlay"></div>
+    <div class="text">Eden Lane</div>
+  </div>
+</div>
+<div class="work-flex-item">
+  <div class="image-container" onClick={() => toggleModal(<Carousel3 />)}>
+    <img src="/Waimarama.jpeg" alt="House in Waimārama" />
+    <div class="static-overlay"></div>
+    <div class="overlay"></div>
+    <div class="text">Waimārama</div>
+  </div>
+</div>
+<div class="work-flex-item">
+  <div class="image-container" onClick={() => toggleModal(<Carousel4 />)}>
+    <img src="/Taihape.jpeg" alt="House in Taihape" />
+    <div class="static-overlay"></div>
+    <div class="overlay"></div>
+    <div class="text">Taihape</div>
+  </div>
+</div>
+<div class="work-flex-item">
+  <div class="image-container" onClick={() => toggleModal(<Carousel5 />)}>
+    <img src="/Interiors.jpeg" alt="Interior pictures" />
+    <div class="static-overlay"></div>
+    <div class="overlay"></div>
+    <div class="text">Interiors</div>
+  </div>
+</div>
+<div class="work-flex-item">
+  <div class="image-container" onClick={() => toggleModal(<Carousel6 />)}>
+    <img src="/Exteriors.jpeg" alt="Exterior pictures" />
+    <div class="static-overlay"></div>
+    <div class="overlay"></div>
+    <div class="text">Exteriors</div>
+  </div>
+</div>
             </div>
           </section>
         </section>

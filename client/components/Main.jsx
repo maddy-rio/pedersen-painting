c2f884beea98d7733f4a7a7ba1c4c4fab9cbd222
Maddy Rio <madison-rio@hotmail.com>
2024-06-25 19:44:45 +1200

    Update mobile landing page photo

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -28,8 +28,9 @@ function Main() {
     <main>
         {/* Landing Page */}
         <section id="landing-page">
-       
+  
           <div class="banner">
+          <div class="overlay"></div>
             <p>Crafting Premium Living, One Brushstroke at a Time.</p>
             <button class="hollow-button" onClick={() => window.location.href = '/work'}>View our work</button>
             <button class="solid-button" onClick={() => window.location.href = '/contact'}>Contact us</button>

eae807bc52b808a58f251a5c526ceaf1ee54e179
Matthew Ranford <matt.ranford16@gmail.com>
2024-10-29 19:00:16 +1300

    Fixing up WAVE for Footer component

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,38 +1,87 @@
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
 
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
 function footer() {
- return (
+  return (
     <section id="section-footer">
-      <div class="footer-container">
-        <div class="footer-item">
-         <p class="footer-heading">Services</p>
-         <ul>
-         <li><a href="/services">Residential</a></li>
-         <li><a href="/services">Commerical</a></li>
-         <li><a href="/services">Wallpapering</a></li>
-         <li><a href="/services">Waterblasting</a></li>
-         </ul>
+      <div className="footer-container">
+        <div className="footer-item">
+          <h2 className="footer-heading">Services</h2>
+          <ul>
+            <li>
+              <a href="/services" aria-label="Residential services">
+                Residential
+              </a>
+            </li>
+            <li>
+              <a href="/services" aria-label="Commercial services">
+                Commerical
+              </a>
+            </li>
+            <li>
+              <a href="/services" aria-label="Wallpapering services">
+                Wallpapering
+              </a>
+            </li>
+            <li>
+              <a href="/services" aria-label="Waterblasting services">
+                Waterblasting
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-item">
+          <h2 className="footer-heading">Quick Links</h2>
+          <ul>
+            <li>
+              <a href="/about">About us</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/work">Our work</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-item">
+          <h2 className="footer-contact">Contact us</h2>
+          <h3 className="footer-number">027 207 1306</h3>
+          <a
+            className="footer-email"
+            href="mailto:jayden@pedersenpainting.co.nz"
+          >
+            jayden@pedersenpainting.co.nz
+          </a>
+          <p className="footer-social">
+            Connect on &nbsp;{' '}
+            <a
+              href="https://www.facebook.com/profile.php?id=100095498316361&mibextid=JRoKGi"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Connect on Facebook"
+            >
+              <FaFacebook />
+            </a>{' '}
+            <a
+              href="https://www.instagram.com/pedersenpaintinganddecorating?igsh=b2NqbTIyOTljdm0="
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Connect on Instagram"
+            >
+              <FaInstagram />
+            </a>
+          </p>
+        </div>
       </div>
-      <div class="footer-item">
-         <p class="footer-heading">Quick Links</p>
-         <ul>
-         <li><a href="/about">About us</a></li>
-         <li><a href="/services">Services</a></li>
-         <li><a href="/work">Our work</a></li>
-         <li><a href="/contact">Contact</a></li>
-         </ul>
+      <div className="footer-overlay"></div>
+      <div className="footer-text">
+        © 2024 Pedersen Painting & Decorating LTD{' '}
       </div>
-      <div class="footer-item">
-         <p class="footer-contact">Contact us</p>
-         <p class="footer-number">027 207 1306</p>
-         <a class="footer-email" href="mailto:jayden@pedersenpainting.co.nz">jayden@pedersenpainting.co.nz</a>
-         <p class="footer-social">Connect on &nbsp; <a href='https://www.facebook.com/profile.php?id=100095498316361&mibextid=JRoKGi' target='_blank'><FaFacebook /></a> <a href='https://www.instagram.com/pedersenpaintinganddecorating?igsh=b2NqbTIyOTljdm0=' target='_blank'><FaInstagram /></a></p>
-      </div>
-      </div>
-       <div class="footer-overlay"></div>
-        <div class="footer-text">© 2024 Pedersen Painting & Decorating LTD </div>
     </section>
- )
+  )
 }
 
-export default footer;
+export default footer
--- client/components/pages/Services.jsx
+++ client/components/pages/Services.jsx
@@ -1,60 +1,101 @@
-import Nav from '../Nav';
-import Footer from '../Footer';
+import Nav from '../Nav'
+import Footer from '../Footer'
 
 function Services() {
   return (
     <>
-<section>
-    <Nav />
-    <h1 className="services-header">&nbsp;&nbsp;Services&nbsp;&nbsp;</h1>
+      <section>
+        <Nav />
+        <h1 className="services-header">&nbsp;&nbsp;Services&nbsp;&nbsp;</h1>
 
-    {/* Residential */}
-    <section id="services-left">
-        <a href='/work'>
-            <img className="services-image" src="/services1.jpeg" alt="House in Waimarama's living room" />
-        </a>
-        <div className="services-text ">
+        {/* Residential */}
+        <section id="services-left">
+          <a href="/work">
+            <img
+              className="services-image"
+              src="/services1.jpeg"
+              alt="House in Waimarama's living room"
+            />
+          </a>
+          <div className="services-text ">
             <p className="section-header">Residential</p>
-            <p>Pedersen Painting consults with clients to understand their vision and preferences. Surfaces are prepared by cleaning, sanding, and priming. Quality paint products are applied using precise techniques to achieve a flawless finish. Attention is given to detail work, such as trim and doors. Once painting is complete, surfaces are cleaned for a pristine result.</p>
-        </div>
-    </section>
+            <p>
+              Pedersen Painting consults with clients to understand their vision
+              and preferences. Surfaces are prepared by cleaning, sanding, and
+              priming. Quality paint products are applied using precise
+              techniques to achieve a flawless finish. Attention is given to
+              detail work, such as trim and doors. Once painting is complete,
+              surfaces are cleaned for a pristine result.
+            </p>
+          </div>
+        </section>
 
-    {/* Commercial */}
-    <section id="services-right">
-        <a href='/work'>
-            <img className="services-image" src="/services2.jpeg" alt="Commercial building exterior" />
-        </a>
-        <div className="services-text ">
+        {/* Commercial */}
+        <section id="services-right">
+          <a href="/work">
+            <img
+              className="services-image"
+              src="/services2.jpeg"
+              alt="Commercial building exterior"
+            />
+          </a>
+          <div className="services-text ">
             <p className="section-header">Commercial</p>
-            <p>For commercial projects, Pedersen Painting assesses the space and plans the painting process accordingly. Surfaces are prepared thoroughly to ensure optimal paint adhesion and durability. Durable paint products are applied to meet the specific needs of commercial environments. Quality checks are conducted throughout to ensure client satisfaction.</p>
-        </div>
-    </section>
+            <p>
+              For commercial projects, Pedersen Painting assesses the space and
+              plans the painting process accordingly. Surfaces are prepared
+              thoroughly to ensure optimal paint adhesion and durability.
+              Durable paint products are applied to meet the specific needs of
+              commercial environments. Quality checks are conducted throughout
+              to ensure client satisfaction.
+            </p>
+          </div>
+        </section>
 
-    {/* Wallpapering */}
-    <section id="services-left">
-        <a href='/work'>
-            <img className="services-image" src="/services3.jpeg" alt="House in Waimarama's bathroom" />
-        </a>
-        <div className="services-text ">
+        {/* Wallpapering */}
+        <section id="services-left">
+          <a href="/work">
+            <img
+              className="services-image"
+              src="/services3.jpeg"
+              alt="House in Waimarama's bathroom"
+            />
+          </a>
+          <div className="services-text ">
             <p className="section-header">Wallpapering</p>
-            <p>Clients collaborate with Pedersen Painting to select the perfect wallpaper design. Walls are cleaned, smoothed, and primed for installation. Our experienced wallpaperer will carefully apply the wallpaper, ensuring perfect alignment and smooth seams. Finishing touches are completed for a professional result.</p>
-        </div>
-    </section>
+            <p>
+              Clients collaborate with Pedersen Painting to select the perfect
+              wallpaper design. Walls are cleaned, smoothed, and primed for
+              installation. Our experienced wallpaperer will carefully apply the
+              wallpaper, ensuring perfect alignment and smooth seams. Finishing
+              touches are completed for a professional result.
+            </p>
+          </div>
+        </section>
 
-    {/* Waterblasting */}
-    <section id="services-right">
-        <img className="services-image" src="/services4.jpeg" alt="Waterblasting exterior of house" />
-        <div className="services-text ">
+        {/* Waterblasting */}
+        <section id="services-right">
+          <img
+            className="services-image"
+            src="/services4.jpeg"
+            alt="Waterblasting exterior of house"
+          />
+          <div className="services-text ">
             <p className="section-header">Waterblasting</p>
-            <p>Before painting exterior surfaces, Pedersen Painting uses waterblasting to clean and prepare the area. This process removes dirt, grime, and old paint layers, creating an ideal surface for paint adhesion. Environmental considerations are taken into account throughout the process.</p>
-        </div>
-    </section>
+            <p>
+              Before painting exterior surfaces, Pedersen Painting uses
+              waterblasting to clean and prepare the area. This process removes
+              dirt, grime, and old paint layers, creating an ideal surface for
+              paint adhesion. Environmental considerations are taken into
+              account throughout the process.
+            </p>
+          </div>
+        </section>
 
-    <Footer />
-</section>
+        <Footer />
+      </section>
     </>
-  );
+  )
 }
 
-export default Services;
-
+export default Services

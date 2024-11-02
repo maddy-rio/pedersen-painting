f9e93a9222d054d76756078bc607d54d47670730
Matthew Ranford <matt.ranford16@gmail.com>
2024-11-03 11:36:47 +1300

    Add initial animtations to Work component

--- client/components/pages/OurWork.jsx
+++ client/components/pages/OurWork.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Nav from '../Nav'
 import Footer from '../Footer'
 import Carousel from './Carousels.jsx/Carousel'
@@ -8,7 +7,38 @@ import Carousel4 from './Carousels.jsx/Carousel4'
 import Carousel5 from './Carousels.jsx/Carousel5'
 import Carousel6 from './Carousels.jsx/Carousel6'
 
+import { motion, animate, stagger, useInView } from 'framer-motion'
+import { useState, useEffect, useRef } from 'react'
+
 function OurWork() {
+  const [hasNewPageAnimated, setHasNewPageAnimated] = useState(false)
+
+  // Animation variants
+  const transitionToNewPageVariant = {
+    hidden: { opacity: 0, y: -5 },
+    visible: { opacity: 1, y: 0 },
+  }
+
+  // Work
+  const images = useRef(null)
+  const imagesInView = useInView(images)
+
+  useEffect(() => {
+    if (imagesInView) {
+      animate(
+        '.animate-project-image',
+        { opacity: 1, x: 0 },
+        {
+          duration: 0.5,
+          delay: stagger(0.35, { startDelay: 0.1 }),
+          ease: 'circOut',
+        },
+      )
+    } else {
+      animate('.animate-project-image', { opacity: 0, x: -10 }, { duration: 0 })
+    }
+  }, [imagesInView])
+
   const [showModal, setShowModal] = useState(false)
   const [selectedCarousel, setSelectedCarousel] = useState(null)
 
@@ -32,11 +62,23 @@ function OurWork() {
     <>
       <Nav />
       <section>
-        <h1 className="page-header">&nbsp;&nbsp;Our work&nbsp;&nbsp;</h1>
-        <section id="work-page">
+        <motion.div
+          variants={transitionToNewPageVariant}
+          initial="hidden"
+          transition={{ ease: 'linear', duration: 0.75, delay: 0.25 }}
+          animate={hasNewPageAnimated ? 'visible' : 'hidden'}
+          onViewportEnter={() => {
+            if (!hasNewPageAnimated) {
+              setHasNewPageAnimated(true)
+            }
+          }}
+        >
+          <h1 className="page-header">&nbsp;&nbsp;Our work&nbsp;&nbsp;</h1>
+        </motion.div>
+        <section id="work-page" ref={images}>
           <section id="work-section">
             <div className="work-flex-container">
-              <div className="work-flex-item">
+              <div className="work-flex-item animate-project-image">
                 <div
                   className="image-container"
                   onClick={() => toggleModal(<Carousel />)}
@@ -54,7 +96,7 @@ function OurWork() {
                   <div className="text">Haumoana</div>
                 </div>
               </div>
-              <div className="work-flex-item">
+              <div className="work-flex-item animate-project-image">
                 <div
                   className="image-container"
                   onClick={() => toggleModal(<Carousel2 />)}
@@ -72,7 +114,7 @@ function OurWork() {
                   <div className="text">Eden Lane</div>
                 </div>
               </div>
-              <div className="work-flex-item">
+              <div className="work-flex-item animate-project-image">
                 <div
                   className="image-container"
                   onClick={() => toggleModal(<Carousel3 />)}
@@ -90,7 +132,7 @@ function OurWork() {
                   <div className="text">Waimārama</div>
                 </div>
               </div>
-              <div className="work-flex-item">
+              <div className="work-flex-item animate-project-image">
                 <div
                   className="image-container"
                   onClick={() => toggleModal(<Carousel4 />)}
@@ -108,7 +150,7 @@ function OurWork() {
                   <div className="text">Taihape</div>
                 </div>
               </div>
-              <div className="work-flex-item">
+              <div className="work-flex-item animate-project-image">
                 <div
                   className="image-container"
                   onClick={() => toggleModal(<Carousel5 />)}
@@ -126,7 +168,7 @@ function OurWork() {
                   <div className="text">Interiors</div>
                 </div>
               </div>
-              <div className="work-flex-item">
+              <div className="work-flex-item animate-project-image">
                 <div
                   className="image-container"
                   onClick={() => toggleModal(<Carousel6 />)}
